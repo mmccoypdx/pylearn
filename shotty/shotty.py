33eb3208e972b35e27b7b7dcd7d786e23fb05ad4
Manjiri Mccoy <manjirim@amazon.com>
2019-01-06 11:41:39 -0800

    Skip snapshots of instances if a snapshot is already in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 # main group
 @click.group()
 def cli():
@@ -103,6 +107,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshots(v):
+                print ("      Skipping snapshot of {0}; has pending snapshot".format(v.id))
+                continue
+
             print("      Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by pylearn")
 
