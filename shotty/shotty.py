aae379613bce1ef78dc4a492975ceae68feddda7
Manjiri Mccoy <manjirim@amazon.com>
2019-01-06 10:42:09 -0800

    Stop instance and wait before snapshot; start after

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -87,13 +87,24 @@ def instances():
 def create_snapshots(project):
     """Create snapshots for EC2 instances"""
 
+    print("Starting snapshots...")
+
     instances = filter_instances(project)
 
     for i in instances:
+        print("   Stopping instance {0}".format(i.id))
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("      Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by pylearn")
+
+        print("   Starting instance {0}".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("All done!")
 
     return
 
